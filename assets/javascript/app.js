ebe072f68de2200f696061197fbb42e702f0a75c
Mark Silva <marksilva@Marks-MacBook-Pro.local>
2019-03-22 20:29:12 -0700

    initial JS pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,67 @@
+
+
+// Home screen with a start button
+
+// When start button is pressed, game begins:
+
+// Home Screen is replaced with the Question Screen
+
+// Question Screen contains a timer counting down, the question and a list of answers
+
+// When the mouse hovers over the question, it changes color
+
+// If the correct answer is selected, Question Screen is replaced with Result Screen
+// informing the player that they are correct along w/ an image
+
+// If the incorrect answer is selected, Question Screen is replaced with Result Screen
+// informing the player that they are wrong accompanied by the correct answer w/ and image
+
+// If the timer runs out, the Queston Screen is replaced by with the Results Scrren 
+// informing the player that time is up accompanied by the correct answer w/ and image
+
+// After 5 seconds, the Result Screen is replaced by another question screen
+
+// When all the questions have been answered, a Score Sceen will replace the final
+// Result Screen displaying the amount the player got right, wrong and unanswered
+
+//------------------------------------------------------
+
+// Home Screen
+// Start button to begin the game
+
+// Screen refresh
+
+
+//------------------------------------------------------
+
+// Question Screen
+// Timer counting down from 20 seconds
+// Question
+// List of answers
+
+// Hover function to make answer light up
+
+// On click function to select answer - 
+// if user selects correct answer, Question Screen goes to Result Screen
+// else if timer runs out, Question Screen goes to Result Screen
+// else Question Screen goes to Result Screen
+
+//------------------------------------------------------
+
+// Result Screen
+// Hidden timer counting down from 5 seconds 
+// when timer is done, next Question Screen displays
+
+// if user chose correct answer
+// "You are correct!" w/ picture
+
+// else if time ran out
+// "Times up! The correct answer is: " w/ picture
+
+// else (user chose wrong)
+// "Wrong! The correct answer is: " w/ picture
+
+//------------------------------------------------------
+
+// Score Screen
+// Display how many correct, wrong and timed out

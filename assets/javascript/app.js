a8ca07e6fdf60029c82981a7d3fa239d78f61f5b
Mark Silva <marksilva@Marks-MacBook-Pro.local>
2019-03-26 22:27:04 -0700

    JS logic updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,19 +17,26 @@
 
 
 //----------------  Varibles  --------------------------------------
- var right = 0;
- var wrong = 0;
- var timeOut = 0;
- var timer = 20;
- var intervalId;
+var right = 0;
+var wrong = 0;
+var resultTime = 5;
+var timer = 21;
+var intervalId;
 //----------------  Questions ---------------------------------------
 // Make an object of questions and answers
 var qNa = {
     one: {
         question: "Who cut off Ned Stark's head?",
-        correctAnswer: "2",
-        options: ["Sandor Clegane","Gregor Clegane","Ilyn Payne","Joffrey Baratheon"],
-        image: '../images/ilynpayne.jpeg' 
+        correctAnswer: "Ilyn Payne",
+        options: ["Sandor Clegane", "Gregor Clegane", "Ilyn Payne", "Joffrey Baratheon"],
+        image: "assets/images/ilynpayne.jpeg"
+    },
+
+    two: {
+        question: "Who is the author of the Game of Thrones?",
+        correctAnswer: "George R. R. Martin",
+        options: ["George R. Martin", "George R. R. Martin", "George R. R. R. Martin", "George R. R. R. R. Martin"],
+        image: "assets/images/ilynpayne.jpeg"
     }
 }
 
@@ -37,47 +44,88 @@ var qNa = {
 //----------------  Home Screen  --------------------------------------
 
 // Start button to begin the game
-    $('.startBtn').on('click', function () {
+$('.startBtn').on('click', function () {
 
-//----------------  Question Screen   ------------------------------------
+    //----------------  Question Screen   ------------------------------------
 
-        // Start button disappears - DONE but would like to add a page divider
-        $('#row5').html('<hr>');
+    // Start button disappears - DONE but would like to add a page divider
+    $('#row5').html('<hr>');
 
-        // Timer counting down from 20 seconds 
-        function countDown () {
-            intervalId = setInterval(decrement, 1000);
+    // Timer counting down from 20 seconds 
+    function countDown() {
+        intervalId = setInterval(decrement, 1000);
+    }
+    function decrement() {
+        timer--;
+        $('#row3').html('<h4>' + timer + '</h4>');
+        if (timer <= 0) {
+            console.log("times up");
+            // if timer runs out, Question Screen goes to Result Screen
+            clearInterval(intervalId);
+            $('#row3').html('<h2>Time\'s Up!</h2>');
+            $('#row4').html('<h3>The correct answer is</h3>');
+            $('#row5').html('<h3>' + qNa.one.correctAnswer + '</h3>');
+            $('#row6').html("<img src='" + qNa.one.image + "'>");
+            $('#row7').html('<hr>');
+            $('#row8').html('<hr>');
+            $('#row9').html('<hr>');
+
         }
-        function decrement () {
-            timer--;
-            $('#row3').html('<h3>' + timer + '</h3>');        
-        }
-        countDown();
-        
-        // Question
-        $('#row4').text(qNa.one.question);
-        // List of answers
-        for (var i = 0; i < qNa.one.options.length; i++) {
-            $('#row' + (6+i)).text(qNa.one.options[i]);      
-        }
+    }
+    // Result screen timer counting down from 5 seconds 
+
+    countDown();
+
+    // Question
+    $('#row4').text(qNa.one.question);
+    // List of answers
+    for (var i = 0; i < qNa.one.options.length; i++) {
+        $('#row' + (6 + i)).text(qNa.one.options[i]);
+    }
+
+
+    // On click function to select answer
+    $('.answers').on('click', function () {
+        var userGuess = $(this).text();
         // if user selects correct answer, Question Screen goes to Result Screen
-        $('.form-check-input').on('click', function() {
-            var userGuess = $(this);
-            if (userGuess === qNa.one.correctAnswer) {
-                console.log("correct");
-       
-            }
-        })
+        if (userGuess === qNa.one.correctAnswer) {
+            console.log("correct");
+            clearInterval(intervalId);
+            $('#row3').html('<h2>Correct!</h2>');
+            $('#row4').html('<hhr>');
+            $('#row5').html('<h3>' + qNa.one.correctAnswer + '</h3>');
+            $('#row6').html("<img src='" + qNa.one.image + "'>");
+            $('#row7').html('<hr>');
+            $('#row8').html('<hr>');
+            $('#row9').html('<hr>');
+        }
+        // else Question Screen goes to Result Screen
+        else {
+            console.log("incorrect");
+            clearInterval(intervalId);
+            $('#row3').html('<h2>Wrong!</h2>');
+            $('#row4').html('<h3>The correct answer is</h3>');
+            $('#row5').html('<h3>' + qNa.one.correctAnswer + '</h3>');
+            $('#row6').html("<img src='" + qNa.one.image + "'>");
+            $('#row7').html('<hr>');
+            $('#row8').html('<hr>');
+            $('#row9').html('<hr>');
+        }
     })
+})
 
 // Hover function to make answer light up
-    $('#answers').hover()
+$('.answers').hover(function () {
+    $(this).toggleClass('active');
+});
 
-// On click function to select answer - 
-
-// else if timer runs out, Question Screen goes to Result Screen
-// else Question Screen goes to Result Screen
-
+// function nextQuestion() {
+//     $('#row4').text(qNa.two.question);
+//     // List of answers
+//     for (var i = 0; i < qNa.two.options.length; i++) {
+//         $('#row' + (6 + i)).text(qNa.two.options[i]);
+//     }
+// }
 
 
 //----------------   Result Screen   --------------------------------------

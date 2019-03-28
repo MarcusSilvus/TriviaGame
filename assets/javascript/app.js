199aebad7a5968eb8db647db06e120475b965eaa
Mark Silva <marksilva@Marks-MacBook-Pro.local>
2019-03-27 22:45:51 -0700

    JS and CSS edits

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,21 +23,50 @@ var timer = 21;
 var intervalId;
 //----------------  Questions ---------------------------------------
 // Make an object of questions and answers
-var qNa = {
-    one: {
+var questionIndex = 0;
+var qNa = [
+    {
         question: "Who cut off Ned Stark's head?",
         correctAnswer: "Ilyn Payne",
         options: ["Sandor Clegane", "Gregor Clegane", "Ilyn Payne", "Joffrey Baratheon"],
         image: "assets/images/ilynpayne.jpeg"
     },
 
-    two: {
+    {
         question: "Who is the author of the Game of Thrones?",
         correctAnswer: "George R. R. Martin",
         options: ["George R. Martin", "George R. R. Martin", "George R. R. R. Martin", "George R. R. R. R. Martin"],
-        image: "assets/images/ilynpayne.jpeg"
+        image: "assets/images/george.jpg"
+    },
+
+    {
+        question: "Which is not one of Daenerys' dragons?",
+        correctAnswer: "Dracarys",
+        options: ["Drogon", "Rhaegal", "Viserion", "Dracarys"],
+        image: "assets/images/dragon.jpg"
+    },
+
+    {
+        question: "Who is Robert Baratheon's biological child?",
+        correctAnswer: "Gendry",
+        options: ["Gendry", "Podrick", "Joffrey", "Jon Snow"],
+        image: "assets/images/gendry.jpg"
+    },
+
+    {
+        question: "Who is Sansa Stark's first husband?",
+        correctAnswer: "Tyrion Lannister",
+        options: ["Ramsay Bolton", "Joffrey Baratheon", "Tyrion Lannister", "Petyr Baelish"],
+        image: "assets/images/sansa.jpeg"
+    },
+
+    {
+        question: "?",
+        correctAnswer: "Dracarys",
+        options: ["Drogon", "Rhaegal", "Viserion", "Dracarys"],
+        image: "assets/images/dragon.jpg"
     }
-}
+]
 
 // $(document).ready(() => {
 //----------------  Home Screen  --------------------------------------
@@ -45,90 +74,83 @@ var qNa = {
 // Start button to begin the game
 $('.startBtn').on('click', function () {
 
-    //----------------  Question Screen   ------------------------------------
-
     // Start button disappears - DONE but would like to add a page divider
     $('#row5').html('<hr>');
-
-    // Timer counting down from 20 seconds 
-    function countDown() {
-        intervalId = setInterval(decrement, 1000);
-    }
-    function decrement() {
-        timer--;
-        $('#row3').html('<h4>' + timer + '</h4>');
-        if (timer <= 0) {
-            timeUp++;
-            // if timer runs out, Question Screen goes to Result Screen
-            clearInterval(intervalId);
-            $('#row3').html('<h2>Time\'s Up!</h2>');
-            $('#row4').html('<h3>The correct answer is</h3>');
-            $('#row5').html('<h3>' + qNa.one.correctAnswer + '</h3>');
-            $('#row6').html("<img src='" + qNa.one.image + "'>");
-            $('#row7').html('<hr>');
-            $('#row8').html('<hr>');
-            $('#row9').html('<hr>');
-
-        }
-    }
-    // Result screen timer counting down from 5 seconds 
-
-    countDown();
-
-    // Question
-    $('#row4').text(qNa.one.question);
-    // List of answers
-    for (var i = 0; i < qNa.one.options.length; i++) {
-        $('#row' + (6 + i)).text(qNa.one.options[i]);
-    }
-
-
-    // On click function to select answer
-    $('.answers').on('click', function () {
-        var userGuess = $(this).text();
-        // if user selects correct answer, Question Screen goes to Result Screen
-        if (userGuess === qNa.one.correctAnswer) {
-            right++;
-            clearInterval(intervalId);
-            $('#row3').html('<h2>Correct!</h2>');
-            $('#row4').html('<hr>');
-            $('#row5').html('<h3>' + qNa.one.correctAnswer + '</h3>');
-            $('#row6').html("<img src='" + qNa.one.image + "'>");
-            $('#row7').html('<hr>');
-            $('#row8').html('<hr>');
-            $('#row9').html('<hr>');
-        }
-        // else Question Screen goes to Result Screen
-        else {
-            wrong++;
-            clearInterval(intervalId);
-            $('#row3').html('<h2>Wrong!</h2>');
-            $('#row4').html('<h3>The correct answer is</h3>');
-            $('#row5').html('<h3>' + qNa.one.correctAnswer + '</h3>');
-            $('#row6').html("<img src='" + qNa.one.image + "'>");
-            $('#row7').html('<hr>');
-            $('#row8').html('<hr>');
-            $('#row9').html('<hr>');
-            nextQuestion();
-            countDown();
-        }
-    })
+    nextQuestion();
+    
 })
 
-// Hover function to make answer light up
-$('.answers').hover(function () {
-    $(this).toggleClass('active');
-});
+// Timer counting down from 20 seconds 
+function countDown() {
+    intervalId = setInterval(decrement, 1000);
+}
+function decrement() {
+    timer--;
+    $('#row3').html('<h4>' + timer + '</h4>');
+    if (timer <= 0) {
+        timeUp++;
+        // if timer runs out, Question Screen goes to Result Screen
+        clearInterval(intervalId);
+        $('#row3').html('<hr>');
+        $('#row4').html('<h3>Time\'s Up! The correct answer is</h3>');
+        $('#row5').html('<h3>' + qNa[questionIndex].correctAnswer + '</h3>');
+        $('#row6').html("<img src='" + qNa[questionIndex].image + "'>");
+        $('#row7').html('<hr>');
+        $('#row8').html('<hr>');
+        $('#row9').html('<hr>');
+        questionIndex++;
+        timer = 21;
+        setTimeout(nextQuestion, 3000);
+    }
+}
 
+//----------------  Question Screen   ------------------------------------
 function nextQuestion() {
-   $('#row4').text(qNa.two.question);
-     // List of answers
-   for (var i = 0; i < qNa.two.options.length; i++) {
-       $('#row' + (6 + i)).text(qNa.two.options[i]);
-       $('#row3').html('<hr>');
-       $('#row5').html('<hr>');
-     }
- }
+    // Question
+    $('#row4').text(qNa[questionIndex].question);
+    // List of answers
+    countDown();
+    for (var i = 0; i < qNa[questionIndex].options.length; i++) {
+        $('#row' + (6 + i)).text(qNa[questionIndex].options[i]);
+        $('#row5').html('<hr>');
+    }
+}
+
+// On click function to select answer
+$('.answers').on('click', function () {
+    var userGuess = $(this).text();
+    // if user selects correct answer, Question Screen goes to Result Screen
+    if (userGuess === qNa[questionIndex].correctAnswer) {
+        right++;
+        clearInterval(intervalId);
+        $('#row3').html('<hr>');
+        $('#row4').html('<h2>Correct!</h2>');
+        $('#row5').html('<h3>' + qNa[questionIndex].correctAnswer + '</h3>');
+        $('#row6').html("<img id='none' src='" + qNa[questionIndex].image + "'>");
+        $('#row7').html('<hr>');
+        $('#row8').html('<hr>');
+        $('#row9').html('<hr>');
+        questionIndex++;
+        timer = 21;
+        setTimeout(nextQuestion, 3000);
+
+    }
+    // else Question Screen goes to Result Screen
+    else {
+        wrong++;
+        clearInterval(intervalId);
+        $('#row3').html('<hr>');
+        $('#row4').html('<h3>Wrong! The correct answer is</h3>');
+        $('#row5').html('<h3>' + qNa[questionIndex].correctAnswer + '</h3>');
+        $('#row6').html("<img src='" + qNa[questionIndex].image + "'>");
+        $('#row7').html('<hr>');
+        $('#row8').html('<hr>');
+        $('#row9').html('<hr>');
+        questionIndex++;
+        timer = 21;
+        setTimeout(nextQuestion, 3000);
+    }
+})
 
 
 //----------------   Score Screen   --------------------------------------

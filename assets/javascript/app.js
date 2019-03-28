f1d6b91361c53c3a1a3ee84f2c4deaadff513721
Mark Silva <marksilva@D-10-19-88-174.dhcp4.washington.edu>
2019-03-27 17:05:43 -0700

    JS edits

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,11 +14,10 @@
 // When all the questions have been answered, a Score Sceen will replace the final
 // Result Screen displaying the amount the player got right, wrong and unanswered
 
-
-
 //----------------  Varibles  --------------------------------------
 var right = 0;
 var wrong = 0;
+var timeUp = 0;
 var resultTime = 5;
 var timer = 21;
 var intervalId;
@@ -59,7 +58,7 @@ $('.startBtn').on('click', function () {
         timer--;
         $('#row3').html('<h4>' + timer + '</h4>');
         if (timer <= 0) {
-            console.log("times up");
+            timeUp++;
             // if timer runs out, Question Screen goes to Result Screen
             clearInterval(intervalId);
             $('#row3').html('<h2>Time\'s Up!</h2>');
@@ -89,10 +88,10 @@ $('.startBtn').on('click', function () {
         var userGuess = $(this).text();
         // if user selects correct answer, Question Screen goes to Result Screen
         if (userGuess === qNa.one.correctAnswer) {
-            console.log("correct");
+            right++;
             clearInterval(intervalId);
             $('#row3').html('<h2>Correct!</h2>');
-            $('#row4').html('<hhr>');
+            $('#row4').html('<hr>');
             $('#row5').html('<h3>' + qNa.one.correctAnswer + '</h3>');
             $('#row6').html("<img src='" + qNa.one.image + "'>");
             $('#row7').html('<hr>');
@@ -101,7 +100,7 @@ $('.startBtn').on('click', function () {
         }
         // else Question Screen goes to Result Screen
         else {
-            console.log("incorrect");
+            wrong++;
             clearInterval(intervalId);
             $('#row3').html('<h2>Wrong!</h2>');
             $('#row4').html('<h3>The correct answer is</h3>');
@@ -110,6 +109,8 @@ $('.startBtn').on('click', function () {
             $('#row7').html('<hr>');
             $('#row8').html('<hr>');
             $('#row9').html('<hr>');
+            nextQuestion();
+            countDown();
         }
     })
 })
@@ -119,28 +120,16 @@ $('.answers').hover(function () {
     $(this).toggleClass('active');
 });
 
-// function nextQuestion() {
-//     $('#row4').text(qNa.two.question);
-//     // List of answers
-//     for (var i = 0; i < qNa.two.options.length; i++) {
-//         $('#row' + (6 + i)).text(qNa.two.options[i]);
-//     }
-// }
+function nextQuestion() {
+   $('#row4').text(qNa.two.question);
+     // List of answers
+   for (var i = 0; i < qNa.two.options.length; i++) {
+       $('#row' + (6 + i)).text(qNa.two.options[i]);
+       $('#row3').html('<hr>');
+       $('#row5').html('<hr>');
+     }
+ }
 
-
-//----------------   Result Screen   --------------------------------------
-
-// Hidden timer counting down from 5 seconds 
-// when timer is done, next Question Screen displays
-
-// if user chose correct answer
-// "You are correct!" w/ picture
-
-// else if time ran out
-// "Times up! The correct answer is: " w/ picture
-
-// else (user chose wrong)
-// "Wrong! The correct answer is: " w/ picture
 
 //----------------   Score Screen   --------------------------------------
 
